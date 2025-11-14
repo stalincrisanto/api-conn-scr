c7c60cfdbeb6d36b3c347487fd8191134853f0de
stalincrisanto <stalincrisanto@hotmail.com>
2025-11-14 16:37:58 -0500

    chore: implementations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataBaseConfig } from './config/database';
+import { AccountAbtModule } from './modules/AccountAbt/account-abt.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataBaseConfig)
+    TypeOrmModule.forRoot(dataBaseConfig),
+    AccountAbtModule
   ],
   controllers: [],
   providers: [],
