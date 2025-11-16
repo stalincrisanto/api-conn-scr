176bd7d73efad15afc73a194bbe01e774bec5948
stalincrisanto <stalincrisanto@hotmail.com>
2025-11-16 07:57:48 -0500

    chore (accountabt): add libreries of nest config and fix config of database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataBaseConfig } from './config/database';
 import { AccountAbtModule } from './modules/AccountAbt/account-abt.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { databaseConfig } from './config/database';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataBaseConfig),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: databaseConfig,
+      inject: [ConfigService],
+    }),
     AccountAbtModule
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('🔥 ENV en AppModule:');
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_PORT:', process.env.DB_PORT);
+    console.log('DB_USER:', process.env.DB_USER);
+    console.log('DB_PASS:', process.env.DB_PASS);
+    console.log('DB_NAME:', process.env.DB_NAME);
+  }
+}
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,30 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+//import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const dataBaseConfig: TypeOrmModuleOptions = {
+//export const dataBaseConfig: TypeOrmModuleOptions = {
+//    type: 'mysql',
+//    host: process.env.DB_HOST,
+//    port: Number(process.env.DB_PORT),
+//    username: process.env.DB_USER,
+//    password: process.env.DB_PASS,
+//    database: process.env.DB_NAME,
+//    // autoLoadEntities: true,
+//    // sincronización en false sporque no se debe usar con la BD del Sistema Central de Recaudo
+//    synchronize: false,
+//    logging: true,
+//}
+
+// config/database.ts
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
+
+export const databaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    // autoLoadEntities: true,
-    // sincronización en false sporque no se debe usar con la BD del Sistema Central de Recaudo
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASS'),
+    database: configService.get('DB_NAME'),
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: false,
     logging: true,
-}
+});
